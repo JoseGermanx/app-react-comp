b8a5e8e4b4c2971b2ada9248f41b4224b49df8dc
Jose G Martinez <jgermanmm@gmail.com>
2023-01-25 16:47:19 -0300

    Se incluye estado que cambia los likes de card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cardwine from './Card';
 import Navigation from './Navbar';
 import {data} from '../data/data.js'
 import styles from './styles/Home.module.css';
 
 export default function Home() {
+
    return (    
     <div >
       <Navigation/>
